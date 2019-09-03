fcade56414e3e90449a0ad175efdededce8a9d97
mohammed-ashiq-m <ashiqmvkpadi@gmail.com>
2019-09-03 11:45:20 +0530

    bug fixed

--- routes/update.js
+++ routes/update.js
@@ -8,18 +8,35 @@ let dbconfig=require('../dbconfig/db-config');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('update');
-    console.log('updationready')
+
 });
 
-router.put('update/:id', function(req,res){
-    const data = req.body;
-    dbconfig.get().collection('signUpForm').update({_id: ObjectId(req.params.id)}, {$set: data},function(err, result){
-        if(err){
-            console.log(err);
+router.post('/', function (req, res) {
+    let firstname = req.body.firstname;
+
+    let id=req.body.id;
+
+
+
+    dbconfig.get().collection('signUpForm').updateOne({"_id":ObjectId(id)},{$set:{firstname}},function (err,result) {
+        if (err) {
+            console.log('error' + err)
+        }else {
+            console.log('successfully updated')
+
+            res.render('feedback')
+
         }
-        res.send('updated successfully');
-    });
-});
+
+    })
+
+
+
+
+
+
+})
+
 
 
 
